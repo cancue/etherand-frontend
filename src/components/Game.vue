90f297b42355a8ff97cde56748c5526c11947c25
cancue <kanqmh@gmail.com>
2019-04-20 14:51:21 +0900

    Add roulette

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <h3 @click="play">
-      {{ entropy }}
+      {{ result }}
     </h3>
+    <div class="roulette-container" @click="start" :style="degree">
+      <img id="roulette" src="https://cdn1.iconfinder.com/data/icons/sin-city-memories/128/roulette-512.png" :class="rotateClass">
+    </div>
   </div>
 </template>
 
@@ -10,30 +13,80 @@
 export default {
   data () {
     return {
-      entropy: "PRESS ME!"
+      isRotating: false,
+      accDec: 0,
+      result: "PRESS ME!",
+    }
+  },
+  computed: {
+    rotateClass () {
+      return this.isRotating ?
+        { rotate: true } : { rotate: true, stop: true }
+    },
+    degree () {
+      return { transform: `rotate(${this.accDec}deg)` }
     }
   },
   methods: {
-    play () {
-      this.getUnsafeEntropy()
-        .then(res => {
-          this.entropy = res
-        })
+    start () {
+      this.isRotating = true
     },
-    async getUnsafeEntropy () {
-      const entropy = await window.onchain.etherand.methods.getUnsafeEntropy.call()
-      return entropy
-    }
+    setResult (deg) {
+      this.isRotating = false
+      this.accDec = this.accDec + 360 + deg
+      this.result = (getRouletteDeg() + this.accDec) % 360
+    },
+    async play () {
+      this.start()
+      const getUnsafeEntropy = window.onchain.etherand.methods.getUnsafeEntropy.call
+      const res = await getUnsafeEntropy()
+      setTimeout(() => this.setResult(Number(res) % 360), 5000)
+    },
   }
+}
+
+const getRouletteDeg = () => {
+  const matrix = $("#roulette").css("transform")
+  let angle = 0
+
+  if (matrix !== "none") {
+    const [a, b] = matrix.split("(")[1].split(")")[0].split(",")
+    angle = Math.round(Math.atan2(b, a) * (180 / Math.PI))
+  }
+
+  return (angle < 0) ? angle + 360 : angle
 }
 </script>
 
 <style lang="scss" scoped>
+$speed: 0.8s;
+
 h3 {
   margin: 2rem;
   padding: 2rem;
   cursor: pointer;
   background-color: gray;
   word-break: break-all;
+}
+.roulette-container {
+  transition: transform 2s ease-out;
+}
+#roulette {
+  cursor: pointer;
+}
+.rotate {
+  animation: spin ($speed * 2) ease-in 1, spin ($speed) linear ($speed * 2) infinite;
+}
+.stop {
+  animation-play-state: paused;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
